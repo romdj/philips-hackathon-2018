66ab7ea80f2c2f318916efef043dbc78eaa5ca7e
Aykut Aras <aykut.aras@philips.com>
2018-11-19 14:06:04 +0100

    Added expressjs and role related UIs

---
+++ README.md
@@ -0,0 +1,16 @@
+# Philips Hackathon 2018
+
+## 1. Configure HSDP IAM Tutorial
+
+1. Create a new sample user
+2. Create a new sample role
+3. Assign permissions to that role (MDM-DEVICEGROUP.CREATE and so on)
+4. Create a new sample group
+5. Assign User to the group
+6. Assign role to the group
+
+## 2. Configure Connector Proposition in MDM Tutorial
+
+## 3. Provision Sample Device
+
+## 4. Process Data and Meaningful Information
---
+++ app/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const path = require('path');
+const port = 3000
+
+// For CDN of css and so
+app.use(express.static('ui'));
+
+// For POST body
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// For HTML as template engine
+app.set('views', path.join(process.cwd() + '/ui/'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get('/roles', (req, res) => {
+  res.render('role-list.html');
+});
+
+app.get('/role-add', (req, res) => {
+  res.render('role-add.html');
+});
+
+app.post('/role-details', (req, res) => {
+  console.log(req.body.name);
+  console.log(req.body.description);
+  console.log(req.body.organization);
+  req.body.id = 'ID_UUID';
+
+  // Create role
+
+  res.render('role-details.html', req.body);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
