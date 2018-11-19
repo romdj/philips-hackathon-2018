d47ca0e41a3e48169966a0c1c907edf6e451a6d3
Aykut Aras <aykut.aras@philips.com>
2018-11-19 14:45:25 +0100

    Added logic for roles

--- app/index.js
+++ app/index.js
@@ -16,8 +16,14 @@ app.set('views', path.join(process.cwd() + '/ui/'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-app.get('/roles', (req, res) => {
-  res.render('role-list.html');
+app.get('/role-list', (req, res) => {
+  const roles = [
+    { name: "Mark", description: "Otto", organization: "@mdo", permissionCount: 12},
+    { name: "Jacob", description: "Thornton", organization: "@fat", permissionCount: 2},
+    { name: "Larry", description: "the Bird", organization: "@twitter", permissionCount: 188}
+  ];
+
+  res.render('role-list.html', {roles});
 });
 
 app.get('/role-add', (req, res) => {
@@ -32,7 +38,23 @@ app.post('/role-details', (req, res) => {
 
   // Create role
 
-  res.render('role-details.html', req.body);
+  const permissions = [
+    { active: false, name: 'ROLE.WRITE', description: 'Role write permission' },
+    { active: false, name: 'ORG.READ', description: 'Read from an org' },
+    { active: true, name: 'MDM.ADMINISTRATOR', description: 'Admin role for MDM' }
+  ];
+
+  res.render('role-details.html', { role: req.body, permissions });
+});
+
+app.post('/permission-activate', (req, res) => {
+  console.log(req.body.role);
+  res.send({success: true});
+});
+
+app.post('/permission-deactivate', (req, res) => {
+  console.log(req.body.role);
+  res.send({success: true});
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
